ebd3b635ef3c75917b3a087748cce13591e05b63
Roshan <roshanbudhathoki02@gmail.com>
2022-02-22 11:41:52 +0545

    added core logic

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+export class SquareComponent {
+  @Input() dataVal!: 'X' | 'O';
+}
